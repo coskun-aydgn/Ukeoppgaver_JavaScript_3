7fada2badfe1ba5771f9920cb6caa91585b1090c
47462 <coskun.aydn@gmail.com>
2024-03-28 17:02:26 +0100

    Initial commit

---
+++ src/main/java/com/example/ukeoppgaver_javascript_3/UkeoppgaverJavaScript3Application.java
@@ -0,0 +1,13 @@
+package com.example.ukeoppgaver_javascript_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkeoppgaverJavaScript3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UkeoppgaverJavaScript3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ukeoppgaver_javascript_3/UkeoppgaverJavaScript3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ukeoppgaver_javascript_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UkeoppgaverJavaScript3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
